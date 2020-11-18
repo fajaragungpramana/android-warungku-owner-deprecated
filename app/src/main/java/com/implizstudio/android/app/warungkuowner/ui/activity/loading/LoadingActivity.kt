32d911a591ac1ecd461ded88e23faf9d0a68f987
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-18 08:59:31 +0700

    Implement activity loading

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/loading/LoadingActivity.kt
@@ -0,0 +1,14 @@
+package com.implizstudio.android.app.warungkuowner.ui.activity.loading
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.implizstudio.android.app.warungkuowner.R
+
+class LoadingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+
+}
