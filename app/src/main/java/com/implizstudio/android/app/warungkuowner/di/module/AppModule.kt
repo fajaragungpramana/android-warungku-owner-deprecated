a97cb852d95b4c3fde8d57098d6191375d39e788
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-18 21:39:09 +0700

    Hilt module only dependency from app

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/di/module/AppModule.kt
@@ -0,0 +1,36 @@
+package com.implizstudio.android.app.warungkuowner.di.module
+
+import android.content.Context
+import android.content.res.Resources
+import com.implizstudio.android.app.warungkuowner.data.local.AppResource
+import com.implizstudio.android.app.warungkuowner.data.repository.AppResourceRepository
+import com.implizstudio.android.app.warungkuowner.data.repository.AppResourceRepositoryImpl
+import com.implizstudio.android.app.warungkuowner.util.TemporarySave
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class AppModule {
+
+    @Provides
+    fun provideContext(@ApplicationContext context: Context) = context
+
+    @Provides
+    fun provideResources(context: Context): Resources = context.resources
+
+    @Provides
+    fun provideTemporarySave(context: Context) = TemporarySave(context)
+
+    @Provides
+    fun provideAppResource(resource: Resources) = AppResource(resource)
+
+    @Provides
+    fun provideAppResourceRepository(appResource: AppResource): AppResourceRepository {
+        return AppResourceRepositoryImpl(appResource)
+    }
+
+}
