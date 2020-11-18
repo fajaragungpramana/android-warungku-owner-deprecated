69344995f2854bf5bfa3ed91a605e14bfe736aee
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-18 21:48:31 +0700

    Create started activity

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/started/StartedActivity.kt
@@ -0,0 +1,14 @@
+package com.implizstudio.android.app.warungkuowner.ui.activity.started
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.implizstudio.android.app.warungkuowner.R
+
+class StartedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_started)
+    }
+
+}
