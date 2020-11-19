fbc0b5a0832ab6aaf32733fd512df8ac142b4d48
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-19 11:12:02 +0700

    Create base activity to replace app compat activity and implement repetitive functions that are used in the activity

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/base/BaseActivity.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/base/BaseActivity.kt
@@ -1,16 +1,36 @@
 package com.implizstudio.android.app.warungkuowner.ui.base
 
 import android.content.Context
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import com.implizstudio.android.app.warungkuowner.util.Language
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
+
+    private lateinit var viewDataBinding: T
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(newBase)
+        Language(resources).setDefault("in")
+    }
 
-        Language(resources).setDefault("en_US")
+    abstract fun getContentView(): Int
+
+    abstract fun onCreated(savedInstanceState: Bundle?)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewDataBinding = DataBindingUtil.setContentView<T>(this, getContentView()).apply {
+            lifecycleOwner = this@BaseActivity
+        }
+
+        onCreated(savedInstanceState)
 
     }
+
+    protected fun getViewDataBinding() = viewDataBinding
 
 }
