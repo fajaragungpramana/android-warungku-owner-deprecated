6772ea5a4cadb2a2bda9628223d3d162f00d9d3f
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-19 15:51:05 +0700

    Implement data binding

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/started/StartedActivity.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/started/StartedActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import com.implizstudio.android.app.warungkuowner.R
 import com.implizstudio.android.app.warungkuowner.databinding.ActivityStartedBinding
 import com.implizstudio.android.app.warungkuowner.ui.base.BaseActivity
+import com.implizstudio.android.app.warungkuowner.util.EventListener
 
 class StartedActivity : BaseActivity<ActivityStartedBinding>() {
 
     override fun getContentView() = R.layout.activity_started
 
     override fun onCreated(savedInstanceState: Bundle?) {
-
+        getViewDataBinding().eventListener = EventListener(this)
     }
 
 }
