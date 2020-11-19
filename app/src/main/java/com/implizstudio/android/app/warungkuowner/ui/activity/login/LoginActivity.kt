24e58ade32404d6579bdf1430d16058b9a1d14f9
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-19 20:48:17 +0700

    Implement action listener goto verification page

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/login/LoginActivity.kt
@@ -3,8 +3,11 @@ package com.implizstudio.android.app.warungkuowner.ui.activity.login
 import android.os.Bundle
 import com.implizstudio.android.app.warungkuowner.R
 import com.implizstudio.android.app.warungkuowner.databinding.ActivityLoginBinding
+import com.implizstudio.android.app.warungkuowner.extension.startActivity
+import com.implizstudio.android.app.warungkuowner.ui.activity.verification.VerificationActivity
 import com.implizstudio.android.app.warungkuowner.ui.base.BaseActivity
 import com.implizstudio.android.app.warungkuowner.util.EventListener
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>() {
 
@@ -12,6 +15,9 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
 
     override fun onCreated(savedInstanceState: Bundle?) {
         getViewDataBinding().eventListener = EventListener(this)
+
+        btn_login.setOnClickListener { startActivity<VerificationActivity>() }
+
     }
 
 }
