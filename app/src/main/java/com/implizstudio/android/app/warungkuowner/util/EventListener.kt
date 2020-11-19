29b9a7fb83576f509356203341e6c63178a2058a
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-19 16:39:46 +0700

    Adding listener navigate to register page

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/util/EventListener.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/util/EventListener.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import com.implizstudio.android.app.warungkuowner.extension.startActivity
 import com.implizstudio.android.app.warungkuowner.ui.activity.login.LoginActivity
+import com.implizstudio.android.app.warungkuowner.ui.activity.register.RegisterActivity
 import javax.inject.Inject
 
 class EventListener @Inject constructor(private val context: Context) {
@@ -15,6 +16,10 @@ class EventListener @Inject constructor(private val context: Context) {
 
     fun gotoLoginPage() {
         context.startActivity<LoginActivity>()
+    }
+
+    fun gotoRegisterPage() {
+        context.startActivity<RegisterActivity>()
     }
 
 }
