a32cf7475110fbd073df1b205e2889eba8af9a81
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-20 16:26:45 +0700

    Adding shared preference key and response code

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/data/model/constant/Constant.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/data/model/constant/Constant.kt
@@ -3,5 +3,9 @@ package com.implizstudio.android.app.warungkuowner.data.model.constant
 object Constant {
 
     const val KEY_IS_INTRODUCED = "is_introduced"
+    const val KEY_OWNER_ID = "owner_id"
+
+    const val HTTP_RESPONSE_CREATED = 201
+    const val HTTP_RESPONSE_NOT_ACCEPTABLE = 406
 
 }
