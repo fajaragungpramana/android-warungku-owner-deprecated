a991e29fe2ebcb4febf644f880f80de829a5bab8
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-20 07:37:53 +0700

    Create register view model to handle data registration

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/register/RegisterViewModel.kt
@@ -0,0 +1,12 @@
+package com.implizstudio.android.app.warungkuowner.ui.activity.register
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.implizstudio.android.app.warungkuowner.data.repository.warungku.WarungKuRepository
+
+class RegisterViewModel @ViewModelInject constructor(private val warungKuRepository: WarungKuRepository) :
+    ViewModel() {
+
+
+
+}
