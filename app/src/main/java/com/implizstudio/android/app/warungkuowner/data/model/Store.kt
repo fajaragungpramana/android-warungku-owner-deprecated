a2ea5730309a85ad5d6a5cf54e2a3928a3be36e1
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-20 10:47:10 +0700

    Update store schema and implement @Bindable

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/data/model/Store.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/data/model/Store.kt
@@ -1,12 +1,16 @@
 package com.implizstudio.android.app.warungkuowner.data.model
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 import com.google.gson.annotations.SerializedName
 
 data class Store(
 
+    @Bindable
     @SerializedName("name")
     var name: String? = null,
 
     @SerializedName("address")
     var address: String? = null
-)
+
+) : BaseObservable()
