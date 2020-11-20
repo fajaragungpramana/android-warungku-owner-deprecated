291142df8ab4c180c1932bbadfe68620a1ac29aa
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-20 10:44:29 +0700

    Create visibility binding adapter by boolean value

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/adapter/BindingAdapter.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/adapter/BindingAdapter.kt
@@ -1,7 +1,10 @@
 package com.implizstudio.android.app.warungkuowner.ui.adapter
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.implizstudio.android.app.warungkuowner.extension.invisible
+import com.implizstudio.android.app.warungkuowner.extension.visible
 
 object BindingAdapter {
 
@@ -9,6 +12,12 @@ object BindingAdapter {
     @BindingAdapter("app:imageSrc")
     fun ImageView.imageSrc(id: Int) {
         setImageResource(id)
+    }
+
+    @JvmStatic
+    @BindingAdapter("app:isShowed")
+    fun View.isShowed(isShowed: Boolean) {
+        if (isShowed) visible() else invisible()
     }
 
 }
