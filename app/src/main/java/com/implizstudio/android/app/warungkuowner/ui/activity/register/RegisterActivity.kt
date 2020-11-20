075f68bc8ae2133862ebee788e16cd61dca20e39
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-20 07:34:44 +0700

    Inject view model

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/register/RegisterActivity.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/register/RegisterActivity.kt
@@ -1,12 +1,17 @@
 package com.implizstudio.android.app.warungkuowner.ui.activity.register
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.implizstudio.android.app.warungkuowner.R
 import com.implizstudio.android.app.warungkuowner.databinding.ActivityRegisterBinding
 import com.implizstudio.android.app.warungkuowner.ui.base.BaseActivity
 import com.implizstudio.android.app.warungkuowner.util.EventListener
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>() {
+
+    private val viewModel by viewModels<RegisterViewModel>()
 
     override fun getContentView() = R.layout.activity_register
 
