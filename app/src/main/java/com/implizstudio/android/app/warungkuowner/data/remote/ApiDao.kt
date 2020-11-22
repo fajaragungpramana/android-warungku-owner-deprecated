dbf17dc3c0288426cb80449a41452b4c37562325
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-22 11:14:06 +0700

    Add a new communicate api endpoint login

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/data/remote/ApiDao.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/data/remote/ApiDao.kt
@@ -2,6 +2,7 @@ package com.implizstudio.android.app.warungkuowner.data.remote
 
 import com.implizstudio.android.app.warungkuowner.data.model.response.OwnerResponse
 import retrofit2.Response
+import retrofit2.http.Field
 import retrofit2.http.FieldMap
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -13,6 +14,13 @@ interface ApiDao {
         @POST("owner/auth/register")
         @FormUrlEncoded
         suspend fun doAccountRegister(@FieldMap data: MutableMap<String, String?>): Response<OwnerResponse>
+
+        @POST("owner/auth/login")
+        @FormUrlEncoded
+        suspend fun doAccountLogin(
+            @Field("email") email: String?,
+            @Field("password") password: String?
+        ): Response<OwnerResponse>
 
     }
 
