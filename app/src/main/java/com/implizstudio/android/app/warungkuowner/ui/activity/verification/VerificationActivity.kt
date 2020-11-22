b4477970e13be297ed7afd10c3211cfb3480434f
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-22 17:24:46 +0700

    Adding email data binding

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/verification/VerificationActivity.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/verification/VerificationActivity.kt
@@ -2,6 +2,7 @@ package com.implizstudio.android.app.warungkuowner.ui.activity.verification
 
 import android.os.Bundle
 import com.implizstudio.android.app.warungkuowner.R
+import com.implizstudio.android.app.warungkuowner.data.model.constant.Constant
 import com.implizstudio.android.app.warungkuowner.databinding.ActivityVerificationBinding
 import com.implizstudio.android.app.warungkuowner.ui.base.BaseActivity
 import com.implizstudio.android.app.warungkuowner.util.EventListener
@@ -11,7 +12,11 @@ class VerificationActivity : BaseActivity<ActivityVerificationBinding>() {
     override fun getContentView() = R.layout.activity_verification
 
     override fun onCreated(savedInstanceState: Bundle?) {
-        getViewDataBinding().eventListener = EventListener(this)
+        getViewDataBinding().let {
+            it.eventListener = EventListener(this)
+            it.email = intent.getStringExtra(Constant.KEY_OWNER_EMAIL)
+        }
+
     }
 
 }
