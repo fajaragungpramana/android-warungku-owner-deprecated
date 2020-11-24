450b31200d88f3ce61596825d8cc86bb67a4c2ac
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-24 08:33:00 +0700

    Handle request to repository

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/verification/VerificationViewModel.kt
@@ -0,0 +1,53 @@
+package com.implizstudio.android.app.warungkuowner.ui.activity.verification
+
+import android.content.Context
+import android.content.Intent
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.implizstudio.android.app.warungkuowner.data.model.Owner
+import com.implizstudio.android.app.warungkuowner.data.model.Verification
+import com.implizstudio.android.app.warungkuowner.data.remote.ApiResult
+import com.implizstudio.android.app.warungkuowner.data.repository.warungku.WarungKuRepository
+import com.implizstudio.android.app.warungkuowner.service.CountDownService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class VerificationViewModel @ViewModelInject constructor(
+    private val context: Context, private val warungKuRepository: WarungKuRepository
+) : ViewModel() {
+
+    private val _isShowProgressBar = MutableLiveData<Boolean>()
+    val isShowProgressBar: LiveData<Boolean>
+        get() = _isShowProgressBar
+
+    private val _responseBody = MutableLiveData<Verification>()
+    val responseBody: LiveData<Verification>
+        get() = _responseBody
+
+    fun doSendVerificationCode(owner: Owner) {
+        context.startService(Intent(context, CountDownService::class.java))
+
+        _isShowProgressBar.value = true
+        GlobalScope.launch {
+
+            when (val result = warungKuRepository.doSendVerificationCode(owner.id, owner.email)) {
+
+                is ApiResult.Success -> {
+                    _isShowProgressBar.postValue(false)
+                    _responseBody.postValue(result.data?.data)
+                }
+
+                is ApiResult.Failure ->
+                    _isShowProgressBar.postValue(false)
+
+                is ApiResult.Error ->
+                    _isShowProgressBar.postValue(false)
+
+            }
+
+        }
+    }
+
+}
