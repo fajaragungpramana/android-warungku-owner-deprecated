330fecc2cce62f762654b18024b5802376737f1d
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-28 22:55:03 +0700

    Adding home view model

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/fragment/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.implizstudio.android.app.warungkuowner.ui.fragment.home
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.implizstudio.android.app.warungkuowner.data.repository.warungku.WarungKuRepository
+
+class HomeViewModel @ViewModelInject constructor(private val warungKuRepository: WarungKuRepository) :
+    ViewModel() {
+
+
+
+}
