ff9603dc4bae793dc6e61ed0f94fa737206188af
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-28 08:44:46 +0700

    Handle action fragment

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/fragment/profile/ProfileFragment.kt
@@ -0,0 +1,18 @@
+package com.implizstudio.android.app.warungkuowner.ui.fragment.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.implizstudio.android.app.warungkuowner.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_profile, container, false)
+
+}
