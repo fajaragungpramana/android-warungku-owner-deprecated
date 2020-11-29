8d615c245ae192c9bfc1b9ba2dbf73b69907383c
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-29 16:58:10 +0700

    Adding currency format and image loader using picasso adapter binding

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/adapter/BindingAdapter.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/adapter/BindingAdapter.kt
@@ -2,9 +2,14 @@ package com.implizstudio.android.app.warungkuowner.ui.adapter
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.implizstudio.android.app.warungkuowner.R
 import com.implizstudio.android.app.warungkuowner.extension.invisible
 import com.implizstudio.android.app.warungkuowner.extension.visible
+import com.squareup.picasso.Picasso
+import java.text.NumberFormat
+import java.util.*
 
 object BindingAdapter {
 
@@ -18,6 +23,21 @@ object BindingAdapter {
     @BindingAdapter("app:isShowed")
     fun View.isShowed(isShowed: Boolean) {
         if (isShowed) visible() else invisible()
+    }
+
+    @JvmStatic
+    @BindingAdapter("app:currencyFormat")
+    fun TextView.currencyFormat(amount: Long) {
+        val format = NumberFormat.getNumberInstance(Locale.getDefault()).format(amount)
+        val currency = "${resources.getString(R.string.rp)} $format"
+
+        text = currency
+    }
+
+    @JvmStatic
+    @BindingAdapter("app:imageUrl")
+    fun ImageView.imageUrl(url: String) {
+        Picasso.get().load(url).into(this)
     }
 
 }
