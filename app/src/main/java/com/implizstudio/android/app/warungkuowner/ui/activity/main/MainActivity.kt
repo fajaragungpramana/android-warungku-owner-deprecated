dd92a61582be3dd67d634b76fd9b950e27757b16
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-29 16:59:55 +0700

    Implement hilt

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/main/MainActivity.kt
@@ -3,7 +3,9 @@ package com.implizstudio.android.app.warungkuowner.ui.activity.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.implizstudio.android.app.warungkuowner.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
