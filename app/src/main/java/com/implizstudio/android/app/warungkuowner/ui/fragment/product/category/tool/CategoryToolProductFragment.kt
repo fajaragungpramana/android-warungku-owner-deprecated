ab6aee1b06b797778ef37f57a5b234014c31406d
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-12-01 00:45:02 +0700

    Handle category data product

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/fragment/product/category/tool/CategoryToolProductFragment.kt
@@ -0,0 +1,18 @@
+package com.implizstudio.android.app.warungkuowner.ui.fragment.product.category.tool
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.implizstudio.android.app.warungkuowner.R
+
+class CategoryToolProductFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_category_product, container, false)
+
+}
