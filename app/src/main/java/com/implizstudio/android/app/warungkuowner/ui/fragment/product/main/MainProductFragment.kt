cbb8c395cb96692a2f084922c10f90ef9afc4680
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-30 08:05:38 +0700

    Action handle product

---
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/fragment/product/main/MainProductFragment.kt
@@ -0,0 +1,16 @@
+package com.implizstudio.android.app.warungkuowner.ui.fragment.product.main
+
+import android.os.Bundle
+import com.implizstudio.android.app.warungkuowner.R
+import com.implizstudio.android.app.warungkuowner.databinding.FragmentMainProductBinding
+import com.implizstudio.android.app.warungkuowner.ui.base.BaseFragment
+
+class MainProductFragment : BaseFragment<FragmentMainProductBinding>() {
+
+    override fun getContentView() = R.layout.fragment_main_product
+
+    override fun onCreated(savedInstanceState: Bundle?) {
+
+    }
+
+}
