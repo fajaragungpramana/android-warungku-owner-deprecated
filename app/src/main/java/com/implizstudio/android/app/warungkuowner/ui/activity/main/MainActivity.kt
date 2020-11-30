7e6498b6857167643aab8f6ba29da2f9e9453d24
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-12-01 00:46:09 +0700

    Set default language for this activity

--- app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/implizstudio/android/app/warungkuowner/ui/activity/main/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.implizstudio.android.app.warungkuowner.ui.activity.main
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.implizstudio.android.app.warungkuowner.R
+import com.implizstudio.android.app.warungkuowner.util.Language
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        Language(resources).setDefault("in")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
